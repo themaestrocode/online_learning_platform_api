e17b42650545b311dada82d24f60b91e7f2f8dd7
Victor Soderu <victorsoderu@gmail.com>
2024-01-24 08:24:26 +0100

    initial commit

---
+++ src/test/java/com/themaestrocode/onlinelearningplatform/api/OnlineLearningPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.themaestrocode.onlinelearningplatform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineLearningPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
